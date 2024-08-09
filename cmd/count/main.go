f17300e907bc22674d11bc506ded63e1e77f8ba5
Vladislav <proskyr.don@gmail.com>
2024-08-09 20:40:16 +0300

    use count package with options in main

--- cmd/count/main.go
+++ cmd/count/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/Zipeer2/count"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/Zipeer2/count"
+)
 
 func main() {
-	count.Main("1\n2\n3\n4")
+	input := bytes.NewBufferString("line1\nline2\nline3\n")
+	output := new(bytes.Buffer)
+
+	c := count.NewCounter(count.WithInput(input), count.WithOutput(output))
+	lines := c.Lines()
+	fmt.Printf("Lines: %d\n", lines)
 }
