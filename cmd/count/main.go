3dd650e34672446c311be95df4ecfe38d7c8786b
Vladislav <proskyr.don@gmail.com>
2024-08-13 14:50:03 +0300

    update main

--- cmd/count/main.go
+++ cmd/count/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-
 	"github.com/Zipeer2/count"
 )
 
 func main() {
-	input := bytes.NewBufferString("line1\nline2\nline3\n")
-	output := new(bytes.Buffer)
-
-	c := count.NewCounter(count.WithInput(input), count.WithOutput(output))
-	lines := c.Lines()
-	fmt.Printf("Lines: %d\n", lines)
+	count.Main()
 }
